7e38aaa76aa0b1314038f7f4bd1890c5ddcc9df4
Quim Rovira <joaquim.rovira@booking.com>
2015-12-13 15:18:42 +0100

    Fix install script and remove an merge conflict left in a makefile (>.<')

--- install_all.sh
+++ install_all.sh
@@ -6,13 +6,13 @@ for REPO in $ALL_REPOS; do
     pushd $REPO >> /dev/null
     echo "Processing ${REPO}..."
     cpanm --installdeps .
-    perl Makefile.PL
-    if make && make test && make install; then
+    if perl Makefile.PL && make && make test && make install; then
         make clean
         echo 'Done!'
     else
         echo "Failed to install $REPO"
         exit 1
     fi
+    popd
     echo "-----------------------------"
 done

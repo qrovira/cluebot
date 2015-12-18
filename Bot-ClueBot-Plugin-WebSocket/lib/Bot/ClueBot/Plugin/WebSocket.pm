fe8b3d2aae3e30bd8b35b898bee66317e9baab2a
Quim Rovira <joaquim.rovira@booking.com>
2015-12-18 19:44:56 +0100

    Update docs for the WebSocket interface a bit

--- Bot-ClueBot-Plugin-WebSocket/lib/Bot/ClueBot/Plugin/WebSocket.pm
+++ Bot-ClueBot-Plugin-WebSocket/lib/Bot/ClueBot/Plugin/WebSocket.pm
@@ -7,18 +7,60 @@ use strict;
 use utf8;
 use 5.14.0;
 
-=head1 VERSION
-
-Version 0.01
-
-=cut
-
-our $VERSION = '0.01';
-
-
 use AnyEvent::Socket qw(tcp_server);
 use AnyEvent::WebSocket::Server;
 use JSON;
+
+our $VERSION = '0.02';
+
+=head1 NAME
+
+Bot::ClueBot::Plugin::WebSocket - ClueBot plugin that provides a websocket interface to the bot's features
+
+=head1 VERSION
+
+Version 0.02
+
+=head1 SYNOPSIS
+
+When enabled, this plugin will listen for websocket connections to a given port (defaults to 9191),
+through which you can execute commands on the bot.
+
+=head1 OPTIONS
+
+=over
+
+=item port
+
+Port on which to listen for connections. Defaults to 9191.
+
+=back
+
+=head1 HELPERS
+
+=over
+
+=item ws_connection( $user )
+
+Returns any websocket connections by the given user.
+
+=item ws_send( $user, $data )
+
+Send a message to the user via webssocket.
+
+=back
+
+=head1 COMMANDS
+
+=over
+
+=item websocket_status
+
+Reports all currently open websocket connections
+
+=back
+
+=cut
 
 sub requires { qw/ Commands WebTokens /; }
 
